a13eac71a71c56795d91cc2ca20027d52cbc3b2e
otto hoffman <otto.hoffman@outlook.com>
2024-03-13 10:45:44 +0100

    uppgift 6 och 7

--- Uppgift.java
+++ Uppgift.java
@@ -2,7 +2,7 @@ public class Uppgift {
     public static void main(String[] args) {
         int[] fält = {5,2,10,6,13,8,16,4,11,3};
         skriv_ut_array(fält);
-        tal_över_åtta(fält);
+        tal_över_8(fält);
 
         /* 
         for (int i = 0; i < fält.length; i++) {
@@ -18,13 +18,16 @@ public class Uppgift {
         }
     }
 
-    static void tal_över_åtta(int[] array)
+    static void tal_över_8(int[] array)
     {
+        int talöver8 = 0;
         System.out.println("__________");
         for (int i= 0; i < array.length; i++) {
             if(array[i]>=8){
                 System.out.println(array[i]);
+                talöver8+=1;
             }
         }
+        System.out.println(talöver8+" tal är över 8");
     }
 }
